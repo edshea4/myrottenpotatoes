f43816301514df4b7bfdef2c32b58cc5ea76c2f5
edshea4 <edshea4@gmail.com>
2016-10-20 16:46:35 +0000

    Finished 4.8 CRUD in the book for this assignment. Cheers!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,5 +11,39 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
     # default: render 'new' template
-  end  
+  end
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+  def create
+    @movie = Movie.create!(user_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  #def update
+  #  @movie = Movie.find params[:id]
+  #  @movie.update_attributes!(user_params)
+  #  flash[:notice] = "#{@movie.title} was successfully updated."
+  #  redirect_to movie_path(@movie)
+  #end
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(user_params)
+    respond_to do |client_wants|
+      client_wants.html {  redirect_to movie_path(@movie)  } # as before
+      client_wants.xml  {  render :xml => @movie.to_xml    }
+    end
+  end
+ private 
+  def user_params
+    params.require(:movie)
+    params[:movie].permit(:title,:rating,:release_date)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 # Seed the RottenPotatoes DB with some movies.
     more_movies = [
-            {:title => 'Aladdin', :rating => 'G',
-            :release_date => '25-Nov-1992'},
             {:title => 'When Harry Met Sally', :rating => 'R',
             :release_date => '21-Jul-1989'},
             {:title => 'The Help', :rating => 'PG-13',
             :release_date => '10-Aug-2011'},
             {:title => 'Raiders of the Lost Ark', :rating => 'PG',
-            :release_date => '12-Jun-1981'}
+            :release_date => '12-Jun-1981'},
+            {:title => 'Full Metal Jacket', :rating => 'R',
+            :release_date => '10-Jul-1987'},
+            {:title => 'Star Wars Episode IV: A New Hope', :rating => 'PG',
+            :release_date => '25-May-1977'}
     ]
 
     more_movies.each do |movie|
